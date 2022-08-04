f8e7cfc0ebbd5e373a0d080ec913129f141d254f
System Administrator <root@Abhijits-MacBook-Air.local>
2022-08-04 00:27:46 -0400

    slidingWindow

---
+++ slidingWindow.js
@@ -0,0 +1,22 @@
+function maxSubarraySum(arr, num) {
+    let maxSum = 0;
+    let tempSum = 0;
+    if (num > arr.length) {
+        return null
+    }
+    for (let i = 0; i < num; i++) {
+        maxSum += arr[i];
+    }
+    tempSum = maxSum;
+    for (let i = num; i < arr.length; i++) {
+        tempSum = tempSum - arr[i - num] + arr[i];
+        if (tempSum > maxSum) {
+            maxSum = tempSum;
+        }
+        // console.log(maxSum)
+    }
+    console.log(maxSum)
+    return maxSum;
+}
+
+maxSubarraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3)
